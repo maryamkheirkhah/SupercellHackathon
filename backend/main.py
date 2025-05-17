908fc4399cf4492f84cbd54b769528f9e56eb61a
Thanh Bui <thanh.v.bui511@gmail.com>
2025-05-17 16:43:30 +0300

    added ui and endpoint for sending prompt

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,28 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class PromptText(BaseModel):
+    prompt: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/prompt")
+def generate_json_design(prompt: PromptText):
+    print(prompt.prompt)
+    return {"prompt": prompt.prompt}
