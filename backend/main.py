c8fc26aaaa14d06f55e1311c6dba46e7e22a38e7
Thanh Bui <thanh.v.bui511@gmail.com>
2025-05-17 22:45:27 +0300

    created figma plugin that calls to the backend

--- backend/main.py
+++ backend/main.py
@@ -26,3 +26,37 @@ def read_root():
 def generate_json_design(prompt: PromptText):
     print(prompt.prompt)
     return {"prompt": prompt.prompt}
+
+@app.post("/figma_plugin")
+def figma_plugin(prompt: PromptText):
+    print(f"prompt text: {prompt.prompt}")
+    return {
+        "frame": {
+            "name": "test",
+            "x": 0,
+            "y": 0,
+            "width": 250,
+            "height": 500,
+            "fills": [{"type": 'SOLID', "color": {"r": 1, "g": 1, "b": 1}}],
+            "children": [
+                {
+                    "type": "rectangle",
+                    "name": "test",
+                    "width": 50,
+                    "height": 50,
+                    "x": 0,
+                    "y": 0,
+                    "fills": [{"type": 'SOLID', "color": {"r": 1, "g": 0.2, "b": 0.9}}],
+                },
+                {
+                    "type": "rectangle",
+                    "name": "test",
+                    "width": 50,
+                    "height": 150,
+                    "x": 100,
+                    "y": 80,
+                    "fills": [{"type": 'SOLID', "color": {"r": 0.5, "g": 1, "b": 1}}],
+                }
+            ]
+        }
+    }
